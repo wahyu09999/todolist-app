db4ff1dce4bce081264c51e1ffd8ffbae8c8ce55
=wahyu09999 <=2041720191@student.polinema.ac.id>
2022-04-27 11:50:18 +0700

    praktikum 1 fitur logout

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,11 @@
 <?php
 
+// use Illuminate\Http\Request;
+// use Illuminate\Support\Facades\Route;
+
 use Illuminate\Http\Request;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\TodoController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +19,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+
 });
